34d6b93481d9506daa9cf0f1f312ced328cfadd4
BalajiMore-tech <83824171+BalajiMore-tech@users.noreply.github.com>
2021-05-20 13:11:36 +0530

    Delete OOPS Assignment directory

--- OOPS Assignment/Question_2.java
+++
@@ -1,8 +0,0 @@
-public class Question_2 
-{
-	public static void main(String[] args)
-	{
-		int rollNo = 100;
-		System.out.println("Roll No = " + rollNo);
-	}
-}
--- OOPS Assignment/Question_4.java
+++
@@ -1,9 +0,0 @@
-public class Question_4 
-{
-	public static void main(String[] args)
-	{
-		byte a = 2, b = 3, c;
-		c = (byte) (a + b);
-		System.out.println(c);
-	}
-}
